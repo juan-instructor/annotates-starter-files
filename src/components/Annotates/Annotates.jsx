a4d73c18ced1232e9d4e1887e94cc569bf1468a3
Juan <juan.instructor@outlook.com>
2025-11-24 18:39:32 -0600

    update starter files

--- src/components/Annotates/Annotates.jsx
+++ src/components/Annotates/Annotates.jsx
@@ -2,11 +2,6 @@ import Form from '../Form/Form';
 import Notes from '../Notes/Notes';
 import { useState } from 'react';
 
-// NOTE: lifting up state. We need to setup the state in Annotates component
-// as both noteForm will set the state of notes
-// and Notes will render the current notes
-// We can use contextApi from React or Redux to manage large contexts.
-// These are known as context stores. We'll learn about it in the future.
 const Annotates = () => {
   const [notes, setNotes] = useState([]);
 
@@ -16,9 +11,6 @@ const Annotates = () => {
     setNotes([...notes, note]);
   };
 
-  // NOTE: prop drilling all the way to Note component -> can become messy really quick!
-  // but there will be old codebases that use this!
-  // Usage of contextApi or Redux which are tools to handle state more maneagable
   const deleteNote = (id) => {
     const noteToDeleteId = id;
 
